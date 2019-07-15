8dde909a20badc441853cc918b754a186e60f3de
Kepa, Mateusz <Mateusz.Kepa@automic.com>
2019-07-15 15:55:16 +0200

    adding simple loops and if cheks

--- hello/hello.go
+++ hello/hello.go
@@ -4,4 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Printf("hello, world\n")
-    }
+	for i := 9999; i < 2000000; i++ {
+		if i%2 == 0 {
+			fmt.Println(i, " jest parzysta")
+		} else if i%i == 0 && i%1 == 0 {
+			fmt.Println(i, " jest liczba pierwsza")
+		}
+	}
+}
